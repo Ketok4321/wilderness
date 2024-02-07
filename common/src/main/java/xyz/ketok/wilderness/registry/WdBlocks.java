012faef2acda550303bea05be4f0d102898b4ea6
Ketok <ketok4321@gmail.com>
2024-02-07 21:19:25 +0100

    Remove bonemealing mossy logs

--- common/src/main/java/xyz/ketok/wilderness/registry/WdBlocks.java
+++ common/src/main/java/xyz/ketok/wilderness/registry/WdBlocks.java
@@ -12,7 +12,6 @@ import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.level.material.PushReaction;
 import xyz.ketok.wilderness.Wilderness;
-import xyz.ketok.wilderness.block.MossyLogBlock;
 import xyz.ketok.wilderness.block.ShelfMushroomBlock;
 
 import java.util.HashMap;
@@ -21,7 +20,7 @@ import java.util.function.Supplier;
 public class WdBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(Wilderness.MOD_ID, Registries.BLOCK);
 
-    public static final RegistrySupplier<MossyLogBlock> MOSSY_OAK_LOG = blockItem("mossy_oak_log", () -> new MossyLogBlock(Properties.copy(Blocks.OAK_LOG)));
+    public static final RegistrySupplier<RotatedPillarBlock> MOSSY_OAK_LOG = blockItem("mossy_oak_log", () -> new RotatedPillarBlock(Properties.copy(Blocks.OAK_LOG)));
     public static final RegistrySupplier<RotatedPillarBlock> MOSSY_OAK_WOOD = blockItem("mossy_oak_wood", () -> new RotatedPillarBlock(Properties.copy(Blocks.OAK_WOOD)));
 
     public static final RegistrySupplier<ShelfMushroomBlock> SHELF_MUSHROOM = blockItem("shelf_mushroom", () -> new ShelfMushroomBlock(Properties.of().mapColor(MapColor.COLOR_BROWN).noCollission().instabreak().sound(SoundType.FUNGUS).pushReaction(PushReaction.DESTROY)));
