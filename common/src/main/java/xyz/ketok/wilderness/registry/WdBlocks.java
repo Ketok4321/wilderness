1ee3cb2a0515e4ae449dd480d3353e93483723f7
Ketok <ketok4321@gmail.com>
2024-02-19 23:04:12 +0100

    Better creative mode tabs

--- common/src/main/java/xyz/ketok/wilderness/registry/WdBlocks.java
+++ common/src/main/java/xyz/ketok/wilderness/registry/WdBlocks.java
@@ -4,10 +4,7 @@ import dev.architectury.hooks.item.tool.AxeItemHooks;
 import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
@@ -21,17 +18,17 @@ import java.util.function.Supplier;
 public class WdBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(Wilderness.MOD_ID, Registries.BLOCK);
 
-    public static final RegistrySupplier<RotatedPillarBlock> OVERGROWN_OAK_LOG = blockItem("overgrown_oak_log", () -> new RotatedPillarBlock(Properties.copy(Blocks.OAK_LOG)), CreativeModeTabs.NATURAL_BLOCKS);
-    public static final RegistrySupplier<RotatedPillarBlock> OVERGROWN_OAK_WOOD = blockItem("overgrown_oak_wood", () -> new RotatedPillarBlock(Properties.copy(Blocks.OAK_WOOD)), CreativeModeTabs.NATURAL_BLOCKS);
+    public static final RegistrySupplier<RotatedPillarBlock> OVERGROWN_OAK_LOG = blockItem("overgrown_oak_log", () -> new RotatedPillarBlock(Properties.copy(Blocks.OAK_LOG)));
+    public static final RegistrySupplier<RotatedPillarBlock> OVERGROWN_OAK_WOOD = blockItem("overgrown_oak_wood", () -> new RotatedPillarBlock(Properties.copy(Blocks.OAK_WOOD)));
 
-    public static final RegistrySupplier<RotatedPillarBlock> OVERGROWN_BIRCH_LOG = blockItem("overgrown_birch_log", () -> new RotatedPillarBlock(Properties.copy(Blocks.BIRCH_LOG)), CreativeModeTabs.NATURAL_BLOCKS);
-    public static final RegistrySupplier<RotatedPillarBlock> OVERGROWN_BIRCH_WOOD = blockItem("overgrown_birch_wood", () -> new RotatedPillarBlock(Properties.copy(Blocks.BIRCH_WOOD)), CreativeModeTabs.NATURAL_BLOCKS);
+    public static final RegistrySupplier<RotatedPillarBlock> OVERGROWN_BIRCH_LOG = blockItem("overgrown_birch_log", () -> new RotatedPillarBlock(Properties.copy(Blocks.BIRCH_LOG)));
+    public static final RegistrySupplier<RotatedPillarBlock> OVERGROWN_BIRCH_WOOD = blockItem("overgrown_birch_wood", () -> new RotatedPillarBlock(Properties.copy(Blocks.BIRCH_WOOD)));
 
-    public static final RegistrySupplier<RotatedPillarBlock> OVERGROWN_SPRUCE_LOG = blockItem("overgrown_spruce_log", () -> new RotatedPillarBlock(Properties.copy(Blocks.SPRUCE_LOG)), CreativeModeTabs.NATURAL_BLOCKS);
-    public static final RegistrySupplier<RotatedPillarBlock> OVERGROWN_SPRUCE_WOOD = blockItem("overgrown_spruce_wood", () -> new RotatedPillarBlock(Properties.copy(Blocks.SPRUCE_WOOD)), CreativeModeTabs.NATURAL_BLOCKS);
+    public static final RegistrySupplier<RotatedPillarBlock> OVERGROWN_SPRUCE_LOG = blockItem("overgrown_spruce_log", () -> new RotatedPillarBlock(Properties.copy(Blocks.SPRUCE_LOG)));
+    public static final RegistrySupplier<RotatedPillarBlock> OVERGROWN_SPRUCE_WOOD = blockItem("overgrown_spruce_wood", () -> new RotatedPillarBlock(Properties.copy(Blocks.SPRUCE_WOOD)));
 
 
-    public static final RegistrySupplier<ShelfMushroomBlock> SHELF_MUSHROOM = blockItem("shelf_mushroom", () -> new ShelfMushroomBlock(Properties.of().mapColor(MapColor.COLOR_BROWN).noCollission().instabreak().sound(SoundType.FUNGUS).pushReaction(PushReaction.DESTROY)), CreativeModeTabs.NATURAL_BLOCKS);
+    public static final RegistrySupplier<ShelfMushroomBlock> SHELF_MUSHROOM = blockItem("shelf_mushroom", () -> new ShelfMushroomBlock(Properties.of().mapColor(MapColor.COLOR_BROWN).noCollission().instabreak().sound(SoundType.FUNGUS).pushReaction(PushReaction.DESTROY)));
 
     public static void setup() {
         AxeItemHooks.addStrippable(OVERGROWN_OAK_LOG.get(), Blocks.OAK_LOG);
@@ -49,9 +46,9 @@ public class WdBlocks {
         }
     }
 
-    private static <T extends Block> RegistrySupplier<T> blockItem(String id, Supplier<T> block, ResourceKey<CreativeModeTab> tab) {
+    private static <T extends Block> RegistrySupplier<T> blockItem(String id, Supplier<T> block) {
         var regBlock = BLOCKS.register(id, block);
-        WdItems.ITEMS.register(id, () -> new BlockItem(regBlock.get(), new Item.Properties().arch$tab(tab)));
+        WdItems.ITEMS.register(id, () -> new BlockItem(regBlock.get(), new Item.Properties()));
 
         return regBlock;
     }
--- common/src/main/java/xyz/ketok/wilderness/registry/WdItems.java
+++ common/src/main/java/xyz/ketok/wilderness/registry/WdItems.java
@@ -1,8 +1,15 @@
 package xyz.ketok.wilderness.registry;
 
+import dev.architectury.registry.CreativeTabOutput;
+import dev.architectury.registry.CreativeTabRegistry;
 import dev.architectury.registry.registries.DeferredRegister;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.ComposterBlock;
 import xyz.ketok.wilderness.Wilderness;
 
@@ -11,5 +18,22 @@ public class WdItems {
 
     public static void setup() {
         ComposterBlock.COMPOSTABLES.put(WdBlocks.SHELF_MUSHROOM.get().asItem(), 0.65F);
+
+        CreativeTabRegistry.modifyBuiltin(BuiltInRegistries.CREATIVE_MODE_TAB.get(CreativeModeTabs.BUILDING_BLOCKS), (FeatureFlagSet flags, CreativeTabOutput output, boolean canUseGameMasterBlocks) -> {
+            output.acceptAfter(Blocks.OAK_WOOD, WdBlocks.OVERGROWN_OAK_LOG.get());
+            output.acceptAfter(WdBlocks.OVERGROWN_OAK_LOG.get(), WdBlocks.OVERGROWN_OAK_WOOD.get());
+            output.acceptAfter(Blocks.BIRCH_WOOD, WdBlocks.OVERGROWN_BIRCH_LOG.get());
+            output.acceptAfter(WdBlocks.OVERGROWN_BIRCH_LOG.get(), WdBlocks.OVERGROWN_BIRCH_WOOD.get());
+            output.acceptAfter(Blocks.SPRUCE_WOOD, WdBlocks.OVERGROWN_SPRUCE_LOG.get());
+            output.acceptAfter(WdBlocks.OVERGROWN_SPRUCE_LOG.get(), WdBlocks.OVERGROWN_SPRUCE_WOOD.get());
+        });
+
+        CreativeTabRegistry.modifyBuiltin(BuiltInRegistries.CREATIVE_MODE_TAB.get(CreativeModeTabs.NATURAL_BLOCKS), (FeatureFlagSet flags, CreativeTabOutput output, boolean canUseGameMasterBlocks) -> {
+            output.acceptAfter(Blocks.OAK_LOG, WdBlocks.OVERGROWN_OAK_LOG.get());
+            output.acceptAfter(Blocks.BIRCH_LOG, WdBlocks.OVERGROWN_BIRCH_LOG.get());
+            output.acceptAfter(Blocks.SPRUCE_LOG, WdBlocks.OVERGROWN_SPRUCE_LOG.get());
+
+            output.acceptAfter(Items.WARPED_FUNGUS, WdBlocks.SHELF_MUSHROOM.get());
+        });
     }
 }
